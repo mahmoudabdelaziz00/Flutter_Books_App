fc3ee6927d1d35de41dd3678f8be180084972aae
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-24 09:06:15 +0300

    add custom app bar to home

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:books_app/Features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,7 +7,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: HomeViewBody(),
     );
   }
 }
