9aa8a9786e25070238010487772de4db4ce01f6c
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-27 15:22:57 +0300

    add title snd subtitle to details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:books_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:books_app/Features/home/presentation/views/widgets/custom_book_image_item.dart';
+import 'package:books_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -14,8 +15,26 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.18),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
             child: CustomBookImageItem(),
+          ),
+          SizedBox(height: 40,),
+          Text(
+            'The Jungle Book',
+            style: Style.textStyle30.copyWith(
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(height: 7,),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Style.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
