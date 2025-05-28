fe7651c89031ed7ca21e2d3257e42b6f297c6f9a
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-28 16:44:36 +0300

    create api service

---
+++ lib/Features/home/data/repos/home_rep_impl.dart
@@ -0,0 +1,16 @@
+import 'package:books_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:books_app/Features/home/data/repos/home_repo.dart';
+import 'package:books_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
