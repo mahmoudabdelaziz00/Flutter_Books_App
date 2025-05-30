f437f9c9c2e21417480afc52bd9d15fe2b4cf7ef
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-30 11:14:38 +0300

    create newest books cubit state

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:books_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
